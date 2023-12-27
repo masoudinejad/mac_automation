6f4450e64d80b51a167315ab7ed94704abb5baa7
Mojtaba Masoudinejad <Mojtaba.Masoudinejad@de.bosch.com>
2023-12-27 13:57:05 +0100

    add brew installations

--- brew.sh
+++ brew.sh
@@ -1,24 +1,42 @@
 #!/usr/bin/env bash
 
+# ---
 # Install Brew Packages
-brew install python
 brew install tree
+brew install latexindent
 
+# ---
 # Install MacOS Applications
-brew cask install google-chrome
-brew cask install firefox
-brew cask install sublime-text
-brew cask install virtualbox
-brew cask install sourcetree
-brew cask install spotify
-brew cask install discord
-brew cask install google-backup-and-sync
-brew cask install skype
-brew cask install gimp
-brew cask install vlc
-brew cask install hyperdock
-brew cask install divvy
+brew cask install --cask warp
+brew cask install --cask brave-browser
+brew cask install --cask rectangle
+brew cask install --cask hiddenbar
+brew cask install --cask alt-tab
+brew cask install --cask appcleaner
+brew cask install --cask mac-mouse-fix
+brew cask install --cask Bitwarden
+brew cask install --cask visual-studio-code
+brew cask install --cask iina
+brew cask install --cask shottr
+brew cask install --cask spotify
+brew cask install --cask skype
+brew cask install --cask zotero
+brew cask install --cask mactex
+brew cask install --cask texstudio
 
-# Install Source Code Pro Font
+# ---
+# Review before install (optional)
+brew cask install --cask cheatsheet
+brew cask install --cask logi-options-plus
+brew cask install --cask applepi-baker
+brew cask install --cask netron
+brew cask install --cask rstudio
+brew cask install --cask quarto
+brew cask install --cask vlc
+brew cask install --cask stats
+
+# ---
+# Install Fonts
 brew tap homebrew/cask-fonts
-brew cask install font-source-code-pro
+brew install --cask font-fira-code
+brew install --cask font-hack-nerd-font
