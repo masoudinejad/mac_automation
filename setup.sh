6f49f5405ccc25e48cee116e345339739dad0d34
Mojtaba Masoudinejad <Mojtaba.Masoudinejad@de.bosch.com>
2024-01-23 18:18:53 +0100

    use single bash to run all

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#  To use do these steps:
+#   chmod +x setup.sh
+#   ./setup.sh
+
+
+# Installing all brew packages
+./brew_setup.sh
+
+# Setup python and environment manager
+./python_setup.sh
+
+# Prepare a proper terminal tooling
+./terminal_setup.sh
