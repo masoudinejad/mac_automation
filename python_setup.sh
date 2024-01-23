470f80200dba74cb80a2ec99e3125b0323f1ae4c
Mojtaba Masoudinejad <Mojtaba.Masoudinejad@de.bosch.com>
2024-01-23 15:55:01 +0100

    add prepare python

---
+++ python_setup.sh
@@ -0,0 +1,18 @@
+
+# Install minicondat
+echo "Installing miniconda."
+brew install --cask miniconda
+conda init "$(basename "${SHELL}")"
+
+# Install pipx
+echo "Installing pipx."
+brew install pipx
+pipx ensurepath
+
+# Install poetry
+echo "Installing poetry."
+pipx install poetry
+
+# Add poetry autocompletion
+mkdir $ZSH_CUSTOM/plugins/poetry
+poetry completions zsh > $ZSH_CUSTOM/plugins/poetry/_poetry
